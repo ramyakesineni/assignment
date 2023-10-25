21400d54f5fa61c795b03fe38d02f06bbc2fc265
ramyakesineni <124325494+ramyakesineni@users.noreply.github.com>
2023-10-25 19:16:44 +0530

    Add files via upload

---
+++ assignment(n).py
@@ -0,0 +1,27 @@
+''' #for loop Code for a table
+n = 5
+for i in range(1,11):
+    print(n,"*",i,"=",n*i) '''  
+
+
+'''#while loop code for a 
+n = 10 
+i = 1
+while(i<= 10):
+    print(n,"*",i,"=",n*i)
+    i+=1 '''
+
+#if/else Using relational operator
+n = 150
+if(n<=100):
+    print(n,"is less han or equal to 100")
+elif(n >100 and n<=200):
+    print(n,"is in range of 100 to 200")
+else:
+    print(n,"s greater than 200")
+ 
+''' #for and if using to print even number
+n = 15
+for i in range (n):
+    if(i%2==0):
+        print(i)'''
